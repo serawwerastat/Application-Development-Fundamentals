4a4f7a9b1f2f60380b4843c5d54671ef49cce437
Artur Filatov <artur.filatov@accenture.com>
2020-01-26 11:03:51 +0200

    [integration] Add integration tests

--- src/main/java/com/fundamentals/controller/UserController.java
+++ src/main/java/com/fundamentals/controller/UserController.java
@@ -21,6 +21,7 @@ import com.fundamentals.models.User;
 public class UserController {
 
     private static final Logger LOG = Logger.getLogger(UserController.class);
+    private static final String USER_NOT_EXIST = "User %s does not exist";
     private static final String ADD_PAGE = "add";
     private static final String LIST_ALL_USERS_PAGE = "list";
     private static final String DELETE_PAGE = "delete";
@@ -43,7 +44,7 @@ public class UserController {
     @GetMapping(value = "/list")
     public String getAllUsers(HttpServletRequest request, HttpServletResponse response) {
         List<User> users = userRepository.findAll();
-        LOG.warn("Get all users result: " + users);
+        LOG.info("Get all users result: " + users);
         List<String> names =
                 users.stream().map(User::getName).collect(Collectors.toList());
         request.setAttribute("userNames", names);
@@ -58,6 +59,10 @@ public class UserController {
 
     @PostMapping(value = "/add")
     public String addUser(HttpServletRequest request, HttpServletResponse response) {
+        validateRequestParameters(request);
+        if(request.getParameter(ERROR_ATTRIBUTE)!=null){
+            return ADD_PAGE;
+        }
         String name = request.getParameter(USER_NAME_ATTRIBUTE);
         String password = request.getParameter(USER_PASS_ATTRIBUTE);
         if (userRepository.findByName(name) != null) {
@@ -83,8 +88,8 @@ public class UserController {
         String name = request.getParameter(USER_NAME_ATTRIBUTE);
         User user = userRepository.findByName(name);
         if (user == null) {
-            request.setAttribute(ERROR_ATTRIBUTE, String.format("User %s does not exist", name));
-            LOG.warn(String.format("User %s does not exist", name));
+            request.setAttribute(ERROR_ATTRIBUTE, String.format(USER_NOT_EXIST, name));
+            LOG.warn(String.format(USER_NOT_EXIST, name));
             return DELETE_PAGE;
         }
         userRepository.delete(user);
@@ -94,7 +99,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/users")
-    public String getUser(@RequestParam(name = USER_NAME_ATTRIBUTE, required = false) String name, HttpServletRequest request,
+    public String getUser(@RequestParam(name = "name", required = false) String name, HttpServletRequest request,
             HttpServletResponse response) {
         if (name == null) {
             LOG.warn("Passed name was null");
@@ -102,8 +107,8 @@ public class UserController {
         }
         User user = userRepository.findByName(name);
         if (user == null) {
-            request.setAttribute(ERROR_ATTRIBUTE, String.format("User %s does not exist", name));
-            LOG.warn(String.format("User %s does not exist", name));
+            request.setAttribute(ERROR_ATTRIBUTE, String.format(USER_NOT_EXIST, name));
+            LOG.warn(String.format(USER_NOT_EXIST, name));
             return FIND_USER_PAGE;
         }
         request.setAttribute(USER_NAME_ATTRIBUTE, user.getName());
@@ -115,6 +120,10 @@ public class UserController {
 
     @PostMapping(value = "/users")
     public String updateUser(HttpServletRequest request, HttpServletResponse response) {
+        validateRequestParameters(request);
+        if(request.getParameter(ERROR_ATTRIBUTE)!=null){
+            return FIND_USER_PAGE;
+        }
         Optional<User> user = userRepository.findById(Integer.valueOf(request.getParameter(USER_ID_ATTRIBUTE)));
         User foundUser;
         if(user.isPresent()){
@@ -125,11 +134,21 @@ public class UserController {
             LOG.warn(String.format("User with ID %s does not exist", request.getParameter(USER_ID_ATTRIBUTE)));
             return FIND_USER_PAGE;
         }
-        foundUser.setPassword(request.getParameter(USER_PASS_ATTRIBUTE)); //TODO FIx bug. Name does not change
+        foundUser.setPassword(request.getParameter(USER_PASS_ATTRIBUTE));
         foundUser.setName(request.getParameter(USER_NAME_ATTRIBUTE));
         userRepository.save(foundUser);
         LOG.info("User was updated. New " + foundUser);
         request.setAttribute("status", foundUser.getName());
         return FIND_USER_PAGE;
+    }
+
+    private void validateRequestParameters(HttpServletRequest request){
+        String name = request.getParameter(USER_NAME_ATTRIBUTE);
+        String password = request.getParameter(USER_PASS_ATTRIBUTE);
+        if(name.isEmpty()|| password.isEmpty()){
+            request.setAttribute(ERROR_ATTRIBUTE, String.format("Fields can not be empty. Passed values: Name [%s]. Password [%s]", name,
+                    password));
+            LOG.warn(String.format("Fields can not be empty. Passed values: Name [%s]. Password [%s]", name,password));
+        }
     }
 }
--- src/test/java/com/fundamentals/ApiDevFundamentalsApplicationTests.java
+++ src/test/java/com/fundamentals/ApiDevFundamentalsApplicationTests.java
@@ -1,20 +1,20 @@
 package com.fundamentals;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.Random;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fundamentals.models.User;
@@ -26,11 +26,12 @@ import net.bytebuddy.utility.RandomString;
 @AutoConfigureMockMvc
 class ApiDevFundamentalsApplicationTests {
 
-    private static final String ERROR_ATTRIBUTE = "error";
+    private static final Logger LOG = Logger.getLogger(ApiDevFundamentalsApplicationTests.class);
     private static final String USER_NAME_ATTRIBUTE = "userName";
     private static final String USER_ID_ATTRIBUTE = "userId";
     private static final String USER_PASS_ATTRIBUTE = "userPassword";
-
+    private User defaultUser1 = new User().setName("BOB").setPassword("1234").setId(999);
+    private User defaultUser2 = new User().setName("JOHN").setPassword("4321").setId(888);
     private User user1;
     private User user2;
 
@@ -44,7 +45,7 @@ class ApiDevFundamentalsApplicationTests {
     private UserRepository userRepository;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         user1 = new User()
                 .setName(RandomString.make(RandomString.DEFAULT_LENGTH))
                 .setPassword(RandomString.make(5));
@@ -55,19 +56,44 @@ class ApiDevFundamentalsApplicationTests {
 
     @Test
     void addUserTest() throws Exception {
+        LOG.info("add test started " + userRepository.findAll());
         mockMvc.perform(post("/add", 42L)
                 .param(USER_NAME_ATTRIBUTE, user1.getName())
                 .param(USER_PASS_ATTRIBUTE, user1.getPassword()))
                 .andExpect(status().isOk());
+        LOG.info("add test finished " + userRepository.findAll());
         User addedUser = userRepository.findByName(user1.getName());
         assertNotNull(addedUser);
         assertThat(addedUser.getPassword(), is(user1.getPassword()));
+        user1.setId(addedUser.getId());
     }
 
     @Test
-    void test(){
-//        assertThat(userRepository.findAll().size(), is(3));
-        System.out.println(userRepository.findAll());
+    void updateUserTest() throws Exception {
+        LOG.info("update test started " + userRepository.findAll());
+        mockMvc.perform(post("/users", 42L)
+                .param(USER_ID_ATTRIBUTE, String.valueOf(defaultUser1.getId()))
+                .param(USER_NAME_ATTRIBUTE, user2.getName())
+                .param(USER_PASS_ATTRIBUTE, user2.getPassword()))
+                .andExpect(status().isOk());
+        LOG.info("upadate test finished " + userRepository.findAll());
+        User addedUser = userRepository.findById(defaultUser1.getId()).orElse(new User());
+        assertNotNull(addedUser.getName());
+        assertNotNull(addedUser.getPassword());
+        assertThat(addedUser.getId(), is(not(0)));
+        assertThat(addedUser.getPassword(), is(user2.getPassword()));
+        assertThat(addedUser.getName(), is(user2.getName()));
+    }
+
+    @Test
+    void deleteUserTest() throws Exception {
+        LOG.info("delete test started " + userRepository.findAll());
+        mockMvc.perform(post("/delete", 42L)
+                .param(USER_NAME_ATTRIBUTE, defaultUser2.getName()))
+                .andExpect(status().isOk());
+        LOG.info("delete test finished " + userRepository.findAll());
+        User addedUser = userRepository.findByName(defaultUser2.getName());
+        assertNull(addedUser);
     }
 
 }
